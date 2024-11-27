ae33b236fea08f64df3623125f603e5a8ce010be
Pkwangho <you@example.com>
2024-11-27 16:43:05 +0900

    2nd commit

---
+++ Helloworld/src/main/java/com/busanit501/helloworld/member/controller/MemberController.java
@@ -0,0 +1,4 @@
+package com.busanit501.helloworld.member.controller;
+
+public class MemberController {
+}
---
+++ Helloworld/src/main/java/com/busanit501/helloworld/member/vo/MemberVO.java
@@ -0,0 +1,28 @@
+package com.busanit501.helloworld.member.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+//롬복 사용하기.
+//@Getter
+//@Setter
+//@ToString
+// VO(Value Object)
+// 사용 용도, 직접적인 데이터베이스에 반영하는 클래스
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class MemberVO {
+    private Long tno;
+    private String name;
+    private String password;
+    private LocalDate birthday;
+    private LocalDate dueDate;
+    private boolean finished;
+}
